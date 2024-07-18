fe2f5be095189668aa31bd1b197c5db2ab6fa0e8
mars4049ai <barinovmarsel77@gmail.com>
2024-07-18 17:18:29 +0300

    app.py

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask, render_template, request
 
+
+
 app = Flask(__name__)
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         pre_mas = float(request.form['mas'])
-        volume3 = round(0.378 * pre_mas, 2)
-        volume4 = round(0.907 * pre_mas, 2)
-        volume5 = round(2.364 * pre_mas, 2)
-        volume6 = round(2.364 * pre_mas, 2)
-        volume7 = round(0.916 * pre_mas, 2)
-        volume8 = round(0.889 * pre_mas, 2)
-        volume9 = round(1.125 * pre_mas, 2)
-        volume10 = round(0.067 * pre_mas, 2)
-        return render_template('index.html', volume3=volume3, volume10=volume10
-                               , volume4=volume4, volume5=volume5, volume6=volume6, volume7=volume7
-                               , volume8=volume8, volume9=volume9)
+        volume_mer = round(0.378 * pre_mas, 2)
+        volume_ven = round(0.907 * pre_mas, 2)
+        volume_mars = round(2.364 * pre_mas, 2)
+        volume_qpt = round(2.364 * pre_mas, 2)
+        volume_ctr = round(0.916 * pre_mas, 2)
+        volume_yrn = round(0.889 * pre_mas, 2)
+        volume_nep = round(1.125 * pre_mas, 2)
+        volume_plt = round(0.067 * pre_mas, 2)
+        return render_template('index.html', volume_mer=volume_mer, volume_plt=volume_plt
+                               , volume_ven=volume_ven, volume_mars=volume_mars, volume_qpt=volume_qpt, volume_ctr=volume_ctr
+                               , volume_yrn=volume_yrn, volume_nep=volume_nep)
     return render_template('index.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
