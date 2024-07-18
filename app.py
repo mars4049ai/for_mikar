860a9775c20ec5678e3f2bad7b00e82a1693d989
mars4049ai <barinovmarsel77@gmail.com>
2024-07-18 17:05:06 +0300

    app.py

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         pre_mas = float(request.form['mas'])
-        volume2 = 27.072 * pre_mas
-        volume3 = 0.378 * pre_mas
-        volume4 = 0.907 * pre_mas
-        volume5 = 0.377 * pre_mas
-        volume6 = 2.364 * pre_mas
-        volume7 = 0.916 * pre_mas
-        volume8 = 0.889 * pre_mas
-        volume9 = 1.125 * pre_mas
-        volume10 = 0.067 * pre_mas
-        return render_template('index.html',  volume2=volume2, volume3=volume3, volume10=volume10
+        volume3 = round(0.378 * pre_mas, 2)
+        volume4 = round(0.907 * pre_mas, 2)
+        volume5 = round(2.364 * pre_mas, 2)
+        volume6 = round(2.364 * pre_mas, 2)
+        volume7 = round(0.916 * pre_mas, 2)
+        volume8 = round(0.889 * pre_mas, 2)
+        volume9 = round(1.125 * pre_mas, 2)
+        volume10 = round(0.067 * pre_mas, 2)
+        return render_template('index.html', volume3=volume3, volume10=volume10
                                , volume4=volume4, volume5=volume5, volume6=volume6, volume7=volume7
                                , volume8=volume8, volume9=volume9)
     return render_template('index.html')
